47bb054808e5801038c0775c4f4127807d67ad0c
ayegbakenneth <ayegbakenneth@gmail.com>
2024-02-06 07:24:00 -0800

    Write a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object.

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""How to implement JSON function for data interchange."""
+
+
+def class_to_json(obj):
+    """Retrieve and show the implementation of a simple data structure of a dictionary."""
+    return obj.__dict__
