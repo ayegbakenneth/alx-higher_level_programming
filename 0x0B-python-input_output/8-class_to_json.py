6c4e9f12549407cc12aabb0c65bf5c43e7574a9f
ayegbakenneth <ayegbakenneth@gmail.com>
2024-02-06 07:28:34 -0800

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,5 +3,5 @@
 
 
 def class_to_json(obj):
-    """Retrieve and show the implementation of a simple data structure of a dictionary."""
+    """Return the implementation of a simple data structure of a dictionary."""
     return obj.__dict__
